829b3ebf2406fe6a5785d365c0bdcffb38a889f2
Bartosz Sosnowski <bartosz@janeasystems.com>
2020-11-17 14:30:37 +0100

    module2

---
+++ RNPDFTEST2/windows/RNPDFTEST2/MainPage.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+
+namespace winrt::RNPDFTEST2::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::RNPDFTEST2::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
+
